cb428a99b1ecc7d4ab661c2b252c57ee95ad74ad
Mojo Talantikite <mojo.talantikite@gmail.com>
2015-12-22 12:46:44 -0500

    Fix networking

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 base:
-	cd centos6-base && packer build template.json
+	cd centos6-base && packer build -force template.json
+
+build-postgres:
+	cd postgres && packer build -force template.json
--- centos6-base/scripts/base.sh
+++ centos6-base/scripts/base.sh
@@ -2,3 +2,10 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 yum -y install  make automake gcc gcc-c++ kernel-devel
 yum -y install htop psmisc sysstat git-core vim curl atop
 yum -y update
+
+# Fix up networking
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
+
+# Cleanup MAC and UUID
+sed -E -i '/^(HWADDR|UUID)/d' /etc/sysconfig/network-scripts/ifcfg-e*
